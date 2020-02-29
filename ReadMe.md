fd8accdfb011ec60ccc40c23fc88d28efe1ba049
Matthew Leonard <leonard.matthewk@gmail.com>
2020-02-29 07:44:58 -0500

    updated html, added ReadMe page

---
+++ ReadMe.md
@@ -0,0 +1,7 @@
+# Password Generator Activity
+
+Password Geneerator is an activity where the user can select from specific criteria to generate a unique password from lowercase letters, uppercase letters, numbers, and special characters, as well as set the length from 8 characters up to 128 characters.
+
+## Process
+
+Upon clicking the Generate Password button the user will be promted to select from the modal Password Criteria to determine their password.  The unique password will be displayed in the div on the orginial page.
--- script.js
+++ script.js
@@ -1,10 +1,36 @@
-  var lowercase = "abcdefghijklmnopqrstuvwxyz";
-  var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  var numbers = "0123456789";
-  var specialCharacters = "!@#$%^&*_+./";
-  var length = "length";
+var lowercase = "abcdefghijklmnopqrstuvwxyz";
+var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numbers = "0123456789";
+var specialCharacters = "!@#$%^&*_+./";
+var length = "length";
 
-  
+var includeLowercase = document.querySelector("includeLowercase");
+var includeUppercase = document.querySelector("includeUppercase");
+var includeNumbers = document.querySelector("IncludeNumbers");
+var includeSpecialCharacters = document.querySelector(
+  "includeSpecialCharacters"
+);
+var length = document.querySelector("legth");
+var confrimEl = document.querySelector("confirm");
+var resultEl = document.querySelector("result");
+var passwordDisplay = document.querySelector("passwordDisplay");
+
+// confirmEl.addEventListener("click", function(event) {
+//   event.preventDefault();
+//   var length = length.value;
+//   var includeLowercase = includeLowercase.checked;
+//   var includeUppercase = includeUppercase.checked;
+//   var includeNumbers = includeNumbers.checked;
+//   var includeSpecialCharacters = includeSpecialCharacters.checked;
+//   var password = passwordGenerator(
+//     length,
+//     includeLowercase,
+//     includeUppercase,
+//     includeNumbers,
+//     includeSpecialCharacters
+//   );
+//   var passwordDisplay = password;
+// });
 
 //   for (var i = 0; i < length; i++) {
 //       var lowercase = Math.floor(Math.random () * lowercase.length);
@@ -12,10 +38,9 @@
 //   }
 //   return password;
 
-
-function getRandomLower(){
-    return String;
-    lowercase[Math.floor(Math.random() * lowercase.length)]
+function getRandomLower() {
+  return String;
+  lowercase[Math.floor(Math.random() * lowercase.length)];
 }
 console.log(getRandomLower());
 // // function passwordGenerator(length, includeLowercase, includeUppercase, includeNumbers, includeSpecialCharacters) {
